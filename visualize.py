2d31d049d73cff5d1127c369142131a427c47381
tron8196 <tron8196@gihub.com>
2021-06-10 22:24:32 +0530

    Added filters for JSON data to remove outliers

--- visualize.py
+++ visualize.py
@@ -2,20 +2,39 @@ import argparse
 from skeleton_sequence import SkeletonSequence
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import os
+from scipy.signal import medfilt
+from scipy.ndimage.filters import gaussian_filter
 skeleton_seq = SkeletonSequence()
 
 parser = argparse.ArgumentParser(description='Visualize skeleton sequence data.')
-parser.add_argument('--data', help='Path to the json recording of an action.')
-args = parser.parse_args()
+parser.add_argument('--data' ,help='Path to the json recording of an action.')
+parser.add_argument('--sigma' ,help='sigma value of the gaussian filter used for smoothening the signal')
+parser.add_argument('--filt_size' ,help='Median Filter size')
 
+args = parser.parse_args()
+print(args)
 assert args.data, "Argument --data is required for loading a recording from a json file."
 skeleton_seq.load_from_json(args.data)
+
+
+
+if args.filt_size is None:
+    filt_size = 3
+else:
+    filt_size = int(args.filt_size)
+
+if args.sigma == '':
+    sigma = 1
+else:
+    sigma = float(args.sigma)
 
 sns.set()
 
 for key, values in skeleton_seq.sequence_data['joint_angles'].items():
+    values = medfilt(volume=values, kernel_size=filt_size)
+    values = gaussian_filter(input=values, sigma=sigma)
     plt.plot(range(len(values)), values)
-
 plt.legend(skeleton_seq.sequence_data['joint_angles'].keys(), ncol=2, loc='upper right');
 plt.show()
+# plt.savefig
